e52e3378ccc9669bf3b9f6177825ed7fe64eafaa
yousuffaraz006 <yousuffaraz006@gmail.com>
2025-04-24 06:57:30 +0530

    Minor error fixed

--- projectapp/pipelines.py
+++ projectapp/pipelines.py
@@ -4,7 +4,7 @@ import uuid
 def create_company(backend, user, response, *args, **kwargs):
     if not Company.objects.filter(user=user).exists():
         url_token = str(uuid.uuid4())
-        Company.objects.create(user=user, url=url_token)
+        Company.objects.create(user=user, url=url_token, webhook_url=url_token)
         emailverifyform = EmailVerification.objects.filter(user=user.email).first()
         if emailverifyform:
             emailverifyform.delete()
